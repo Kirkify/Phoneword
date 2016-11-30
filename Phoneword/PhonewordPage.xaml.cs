06855147384a9947da59d56341bec0fc081cc14c
Kirkify <davi0534@algonquinlive.com>
2016-11-30 16:54:15 -0500

    Project complete, translates any alphabetic letters to numbers then allows users to initiate a call to that number, works on android or ios

--- Phoneword/PhonewordPage.xaml.cs
+++ Phoneword/PhonewordPage.xaml.cs
@@ -1,12 +1,41 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Phoneword
 {
 	public partial class PhonewordPage : ContentPage
 	{
+		private string translatedNumber;
+
 		public PhonewordPage()
 		{
 			InitializeComponent();
+
+			this.Padding = new Thickness(20,
+										Device.OnPlatform<double>(40, 20, 20), 
+			                             20, 20);
+		}
+
+		void OnTranslate(object sender, System.EventArgs e)
+		{
+			translatedNumber = PhonewordTranslator.ToNumber(EnteredNumber.Text);
+
+			if (translatedNumber != null)
+			{
+				CallBtn.IsEnabled = true;
+				CallBtn.Text = string.Format("Call {0}", translatedNumber);
+			}
+			else
+			{
+				CallBtn.IsEnabled = false;
+				EnteredNumber.Text = string.Empty;
+				CallBtn.Text = "Call";
+			}
+		}
+
+		void MakeCall(object sender, System.EventArgs e)
+		{
+			Device.OpenUri(new Uri("tel:" + translatedNumber));
 		}
 	}
 }
